7bee049996445a8c3a98d68e998f830e3a89b431
José Carlos Muñoz Méndez <jmunozm@alsea.com.mx>
2019-03-28 15:57:20 -0600

    Variables de entorno

--- src/components/listCards.vue
+++ src/components/listCards.vue
@@ -1,11 +1,6 @@
   <template>
   <div class="container">
     <div class="row">
-      <pre>
-        {{ environment }}
-      </pre>
-    </div>
-    <div class="row">
       <div class="col-12">
       </div>
     </div>
